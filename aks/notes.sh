281daf4c445240d56ab55900676a23ef39be4856
Denis Sinelshchikov <denis_sinelshchikov@tjx.com>
2023-01-24 17:19:19 -0500

    updated notes

--- aks/notes.sh
+++ aks/notes.sh
@@ -1,6 +1,10 @@
+# Initial ENV vars
+
 export RESOURCE_GROUP=teamResources
 export CLUSTER_NAME=aks-team12-cluser
 export LOCATION="South Central US"
+
+# Deploy the AKS cluster
 
 az aks create \
     --resource-group $RESOURCE_GROUP \
@@ -13,27 +17,24 @@ az aks create \
     --enable-aad
     --attach-acr registryhrh2972
 
-az acr login --name registryhrh2972
-
-az aks update \
-    --resource-group $RESOURCE_GROUP \
-    --name $CLUSTER_NAME \
-    --attach-acr registryhrh2972
+# Generating kube config to access the cluster
 
 az aks get-credentials --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP
 
+# Setting up AKS secret for MSSQL
 
 kubectl create secret generic sql \
 --from-literal=sqluser="sqladminhRh2972" \
 --from-literal=sqlpassword="Password@2345"
 
-kubectl port-forward service/trips-service 28015:80
+# Verifications of deployments and services
 
 kubectl port-forward deployment/poi 28015:80
 kubectl port-forward deployment/user-java 28015:80
 kubectl port-forward deployment/userprofile 28015:80
 kubectl port-forward deployment/tripviewer 28015:80
 
+kubectl port-forward service/trips-service 28015:80
 kubectl port-forward service/userprofile-service 28015:80
 kubectl port-forward service/user-java-service 28015:80
 kubectl port-forward service/poi-service 28015:80
